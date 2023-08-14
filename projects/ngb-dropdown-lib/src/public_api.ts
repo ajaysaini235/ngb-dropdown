0710592aa19b6d0aa50a3316890cc607e78772e1
Ajay Saini <sainiajay235@gmail.com>
2023-08-14 19:45:18 +0530

    initial commit

---
+++ projects/ngb-dropdown-lib/src/public_api.ts
@@ -0,0 +1,4 @@
+export { AngularMultiSelect, AngularMultiSelectModule } from './lib/multiselect.component';
+export * from './lib/clickOutside';
+export { ListFilterPipe } from './lib/list-filter';
+export * from './lib/menu-item';
